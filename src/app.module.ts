fc91e796c63df1e40566752561185f6fef4a7027
Tim Hoevenaar <tjwr.hoevenaar@student.avans.nl>
2025-10-06 10:00:18 +0200

    cat example toegevoegd
    - cat example toegevoegd met voorbeeld code

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { CatsModule } from './cats/cats.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [MongooseModule.forRoot('mongodb://localhost/nest'), CatsModule, AuthModule, UsersModule],
 })
 export class AppModule {}
+
