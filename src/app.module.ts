27a3c86304ec13d4398ba48b87afed4800e2d50c
Tim Hoevenaar <tjwr.hoevenaar@student.avans.nl>
2025-10-07 10:32:01 +0200

    database werkend
    - database werkend
    - je kan nu data van cats via postman in mongoDB zetten

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-// import { MongooseModule } from '@nestjs/mongoose';
-// import { CatsModule } from './cats/cats.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { CatsModule } from './cats/cats.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 
@@ -10,10 +10,8 @@ import { UsersModule } from './users/users.module';
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    // MongooseModule.forRoot(process.env.MONGODB_URI!, {
-    //   connectionName: 'default',
-    // }),
-    // CatsModule,
+    MongooseModule.forRoot(process.env.MONGODB_URI!),
+    CatsModule,
     AuthModule,
     UsersModule,
   ],
